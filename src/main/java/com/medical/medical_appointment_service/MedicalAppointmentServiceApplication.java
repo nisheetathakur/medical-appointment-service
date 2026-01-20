9e9201481dd2fa685dab734c017166ff1463db09
Nisheeta Thakur <nish@MacBook-Air-de-Nisheeta.local>
2026-01-20 12:34:11 +0530

    Fix project structure (removed nested folder)

---
+++ src/main/java/com/medical/medical_appointment_service/MedicalAppointmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medical.medical_appointment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalAppointmentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medical/medical_appointment_service/MedicalAppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.medical_appointment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
